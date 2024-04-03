48642ba7e1df25ccaef61b5f20f31fe75c23f96a
Spades749 <147041329+Spades749@users.noreply.github.com>
2024-04-03 11:46:50 +0200

    UPDATE + COMPTEUR

--- saut.py
+++ saut.py
@@ -9,12 +9,21 @@ pygame.display.set_caption('saut mdrr ca rebondit')
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+GREY = (128, 128, 128)
+font = pygame.font.Font('Daydream.ttf', 25)
 
 player_pos = [400, 500]
 jump_speed = -20
 gravity = 1
 in_jump = False
 y_speed = 0
+score = 0
+score_pos = [25, 25]
+
+def Show_score():
+    score_display = font.render(str(score), 1, (0, 255, 0))
+    screen.blit(score_display, score_pos)
 
 running = True
 while running:
@@ -24,6 +33,7 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not in_jump:
+                score = score + 1
                 in_jump = True
                 y_speed = jump_speed
 
@@ -38,6 +48,8 @@ while running:
 
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, (*player_pos, 50, 50))
+    pygame.draw.rect(screen, GREY, (*score_pos, 150, 50), border_radius=10)
+    Show_score()
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
