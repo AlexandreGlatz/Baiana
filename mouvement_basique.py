ee4597c05b85146dea04403a030ec071fa128ebd
SofianeSaada <147041366+SofianeSaada@users.noreply.github.com>
2024-04-04 10:57:45 +0200

    tirs rajouter

--- mouvement_basique.py
+++ mouvement_basique.py
@@ -3,28 +3,34 @@ import sys
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win_width, win_height = 500, 500
+win = pygame.display.set_mode((win_width, win_height))
 
-x = 50
-y = 50
-width = 60
-height = 60
-vel = 300
+player_cube_width, player_cube_height = 60, 60
+player_cube_vel = 300
+
+enemy_cube_width, enemy_cube_height = 60, 60
+enemy_cube_vel = 100
+
+player_cube = pygame.Rect((win_width - player_cube_width) // 2, win_height - player_cube_height, player_cube_width, player_cube_height)
+enemy_cube = pygame.Rect((win_width - enemy_cube_width) // 2, 20, enemy_cube_width, enemy_cube_height)
+
+bullet_speed = 500
+bullet_interval = 2
+bullet_timer = 0
+
+bullet_list = []
 
 clock = pygame.time.Clock()
 FPS = 60
 
-jump_speed = -20
-gravity = 1
-in_jump = False
-y_speed = 0
+class Bullet:
+    def __init__(self, x, y, vel):
+        self.rect = pygame.Rect(x, y, 10, 10)
+        self.vel = vel
 
-def jump():
-    global y, y_speed, in_jump
-    if not in_jump:
-        in_jump = True
-        y_speed = jump_speed
-
+    def move(self, dt):
+        self.rect.y += self.vel * dt
 running = True
 
 while running:
@@ -36,25 +42,30 @@ while running:
 
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_q]:
-        x -= vel * dt
+    if keys[pygame.K_LEFT]:
+        player_cube.x -= player_cube_vel * dt
 
-    if keys[pygame.K_d]:
-        x += vel * dt
+    if keys[pygame.K_RIGHT]:
+        player_cube.x += player_cube_vel * dt
 
-    if keys[pygame.K_SPACE]:
-        jump()
-
-    if in_jump:
-        y += y_speed
-        y_speed += gravity
-        if y > 500:
-            y = 500
-            in_jump = False
-            y_speed = 0
+    bullet_timer += dt
+    if bullet_timer >= bullet_interval:
+        new_bullet = Bullet(enemy_cube.x + enemy_cube_width // 2, enemy_cube.y + enemy_cube_height, bullet_speed)
+        bullet_list.append(new_bullet)
+        bullet_timer = 0
 
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 255, 255), (x, y, width, height))
+    pygame.draw.rect(win, (255, 255, 255), player_cube)
+    pygame.draw.rect(win, (255, 0, 0), enemy_cube)
+
+    for bullet in bullet_list:
+        bullet.move(dt)
+        pygame.draw.rect(win, (0, 255, 0), bullet.rect)
+
+        # Supprimer les balles qui sortent de l'écran
+        if bullet.rect.y < 0 or bullet.rect.y > win_height:
+            bullet_list.remove(bullet)
+
     pygame.display.update()
 
 pygame.quit()
