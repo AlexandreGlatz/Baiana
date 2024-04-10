9901d6d7274aa9fe59919cb9de405aa01c3cef7a
Spades749 <mfournier@gaming.tech>
2024-04-11 00:56:32 +0200

    update : toutes les animations

--- saut.py
+++ saut.py
@@ -29,27 +29,216 @@ except FileNotFoundError:
 sprite_sheet_path = 'run_sheet.png'
 sprite_sheet = pygame.image.load(sprite_sheet_path).convert_alpha()
 scale_factor = 0.3
+scale_m_factor = 0.4
+scale_m2_factor = 0.4
+scale_d_factor = 0.4
+scale_d2_factor = 0.4
+scale_d3_factor = 0.4
 
 jump_sheet_path = 'jump-Sheet.png'
 jump_sheet = pygame.image.load(jump_sheet_path).convert_alpha()
+
+villain_sheet_path ='mechant.png'
+villain_sheet = pygame.image.load(villain_sheet_path).convert_alpha()
+
+villain2_sheet_path ='mechant2.png'
+villain2_sheet = pygame.image.load(villain2_sheet_path).convert_alpha()
+
+dancer_sheet_path = 'dancer.png'
+dancer_sheet = pygame.image.load(dancer_sheet_path).convert_alpha()
+
+dancer2_sheet_path = 'dancer2.png'
+dancer2_sheet = pygame.image.load(dancer2_sheet_path).convert_alpha()
+
+dancer3_sheet_path = 'dancer3.png'
+dancer3_sheet = pygame.image.load(dancer3_sheet_path).convert_alpha()
 
 # Cutting sprite sheet frames
 frame_width = 512
 frame_height = 700
 running_frames = [sprite_sheet.subsurface(pygame.Rect(i * frame_width, 0, frame_width, frame_height)) for i in range(4)]
 
+    #jump
 frame_j_width = 500
 frame_j_height = 700
 jumping_frames = [jump_sheet.subsurface(pygame.Rect(i * frame_j_width, 0, frame_j_width, frame_j_height)) for i in range(5)]
+
+    #mechant
+frame_m_width = 200
+frame_m_height = 700
+mechant_frames = [villain_sheet.subsurface(pygame.Rect(i * frame_m_width, 0, frame_m_width, frame_m_height)) for i in range(13)]
+
+    #mechant2
+frame_m2_width = 200
+frame_m2_height = 700
+mechant2_frames = [villain2_sheet.subsurface(pygame.Rect(i * frame_m2_width, 0, frame_m2_width, frame_m2_height)) for i in range(13)]
+
+    #dancer
+frame_d_width = 210
+frame_d_height = 700
+dancer_frames = [dancer_sheet.subsurface(pygame.Rect(i * frame_d_width, 0, frame_d_width, frame_d_height)) for i in range(2)]
+
+    #dancer2
+frame_d2_width = 210
+frame_d2_height = 700
+dancer2_frames = [dancer2_sheet.subsurface(pygame.Rect(i * frame_d2_width, 0, frame_d2_width, frame_d2_height)) for i in range(2)]
+
+    #dancer3
+frame_d3_width = 210
+frame_d3_height = 700
+dancer3_frames = [dancer3_sheet.subsurface(pygame.Rect(i * frame_d3_width, 0, frame_d3_width, frame_d3_height)) for i in range(2)]
 
 # Resizing frames
 scaled_frame_width = int(frame_width * scale_factor)
 scaled_frame_height = int(frame_height * scale_factor)
 running_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_width, scaled_frame_height)) for frame in running_frames]
 
+    #jump
 scaled_frame_j_width = int(frame_j_width * scale_factor)
 scaled_frame_j_height = int(frame_j_height * scale_factor)
 jumping_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_j_width, scaled_frame_j_height)) for frame in jumping_frames]
+
+    #mechant
+scaled_frame_m_width = int(frame_m_width * scale_m_factor)
+scaled_frame_m_height = int(frame_m_height * scale_m_factor)
+mechant_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_m_width, scaled_frame_m_height)) for frame in mechant_frames]
+
+    #mechant2
+scaled_frame_m2_width = int(frame_m2_width * scale_m2_factor)
+scaled_frame_m2_height = int(frame_m2_height * scale_m2_factor)
+mechant2_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_m2_width, scaled_frame_m2_height)) for frame in mechant2_frames]
+
+    #dancer
+scaled_frame_d_width = int(frame_d_width * scale_d_factor )
+scaled_frame_d_height = int(frame_d_height * scale_d_factor)
+dancer_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_d_width, scaled_frame_d_height)) for frame in dancer_frames]
+
+    #dancer2
+scaled_frame_d2_width = int(frame_d2_width * scale_d2_factor)
+scaled_frame_d2_height = int(frame_d2_height * scale_d2_factor)
+dancer2_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_d2_width, scaled_frame_d2_height)) for frame in dancer2_frames]
+
+    #dancer3
+scaled_frame_d3_width = int(frame_d3_width * scale_d3_factor)
+scaled_frame_d3_height = int(frame_d3_height * scale_d3_factor)
+dancer3_frames_scaled = [pygame.transform.scale(frame, (scaled_frame_d3_width, scaled_frame_d3_height)) for frame in dancer3_frames]
+
+all_sprites = pygame.sprite.Group()
+
+# Villain Class
+class Villain(pygame.sprite.Sprite):
+    def __init__(self, pos, frames):
+        super().__init__()
+        self.frames = frames
+        self.current_frame = 0
+        self.image = frames[self.current_frame]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.animation_time = 0.15
+        self.current_time = 0
+
+    def update(self, dt):
+        # Animation loop
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
+villain = Villain([400, 50], mechant_frames_scaled)
+
+all_sprites.add(villain)
+
+# Villain2 Class
+class Villain2(pygame.sprite.Sprite):
+    def __init__(self, pos, frames):
+        super().__init__()
+        self.frames = frames
+        self.current_frame = 0
+        self.image = frames[self.current_frame]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.animation_time = 0.15
+        self.current_time = 0
+
+    def update(self, dt):
+        # Animation loop
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
+villain2 = Villain2([300, 50], mechant2_frames_scaled)
+
+all_sprites.add(villain2)
+
+# Dancer Class
+class Dancer(pygame.sprite.Sprite):
+    def __init__(self, pos, frames):
+        super().__init__()
+        self.frames = frames
+        self.current_frame = 0
+        self.image = frames[self.current_frame]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.animation_time = 0.15
+        self.current_time = 0
+
+    def update(self, dt):
+        # Animation loop
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
+dancer = Dancer([200, 50], dancer_frames_scaled)
+
+all_sprites.add(dancer)
+
+# Dancer2 Class
+class Dancer2(pygame.sprite.Sprite):
+    def __init__(self, pos, frames):
+        super().__init__()
+        self.frames = frames
+        self.current_frame = 0
+        self.image = frames[self.current_frame]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.animation_time = 0.15
+        self.current_time = 0
+
+    def update(self, dt):
+        # Animation loop
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
+dancer2 = Dancer2([100, 50], dancer2_frames_scaled)
+
+all_sprites.add(dancer2)
+
+# Dancer3 Class
+class Dancer3(pygame.sprite.Sprite):
+    def __init__(self, pos, frames):
+        super().__init__()
+        self.frames = frames
+        self.current_frame = 0
+        self.image = frames[self.current_frame]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.animation_time = 0.15
+        self.current_time = 0
+
+    def update(self, dt):
+        # Animation loop
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
+dancer3 = Dancer3([0, 50], dancer3_frames_scaled)
+
+all_sprites.add(dancer3)
 
 # Player class
 class Player(pygame.sprite.Sprite):
@@ -142,8 +331,8 @@ class Camera:
             return entity.rect.move(-self.rect.topleft[0], -self.rect.topleft[1])
 
 player = Player([400, 405], running_frames_scaled, jumping_frames_scaled)
+all_sprites.add(player)
 camera = Camera(player, wind_size[0], wind_size[1])
-all_sprites = pygame.sprite.Group(player)
 
 # Decor element (static)
 decor_element = pygame.Surface((100, 50))
